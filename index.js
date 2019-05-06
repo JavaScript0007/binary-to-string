297b9b02e0f2d5bd278c0108a9a9404bad6b4d92
americanman <snowboardracer7@gmail.com>
2019-05-05 21:17:00 -0400

    Init commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const binaryAgent = (str) => {
+  str = str.split(" ")
+  let newMap = str.map(item => String.fromCharCode(parseInt(item, 2)))
+  newMap = newMap.join("")
+  return newMap
+}
+
+console.log(binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111"))
